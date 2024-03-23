567e9ffa723175ec878d78ae1baa6fe03f1fcba6
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-03-23 16:06:14 +0300

    EmployeeController Add Method and EmployeeRepository Add Method added.

---
+++ Crud_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Crud_WebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeRepository _employeeRepository;
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            this._employeeRepository = employeeRepository;  
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee([FromBody]Employee employee)
+        {
+            await _employeeRepository.AddEmployee(employee);
+            return Ok();
+        }
+
+    }
+
+}
---
+++ Crud_WebAPI/Data/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Crud_WebAPI.Data
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+        public async Task AddEmployee(Employee employee)
+        {
+            await _appDbContext.Set<Employee>().AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
